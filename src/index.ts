643929854a5dbed56cf391137a35bcc49fdf1a78
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-11 12:10:50 +0530

    setup json-server to store users

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import User from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Spider Man', age: 20 });
-
-user.on('change', () => {
-  console.log('Change #1 trigged!');
-});
-
-user.on('click', () => {
-  console.log('Click #1 trigged!');
-});
-
-user.on('change', () => {
-  console.log('Change #2 triggered!');
-});
-
-user.on('save', () => {
-  console.log('Save was triggered!');
-});
-
-// No error on trigger any random unregistered event
-user.trigger('dsjhfkjds');
+axios.get('http://localhost:3000/users/1');

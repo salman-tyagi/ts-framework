d777e8b8051e0e3074444de0cfee0a4dd4cfd356
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-11 10:57:39 +0530

    added: triggered method after registering the event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,21 @@ import User from './models/User';
 
 const user = new User({ name: 'Spider Man', age: 20 });
 
-user.on('change', () => {});
-user.on('click', () => {});
-user.on('change', () => {});
-user.on('randomEvent', () => {});
+user.on('change', () => {
+  console.log('Change #1 trigged!');
+});
 
-console.log(user);
+user.on('click', () => {
+  console.log('Click #1 trigged!');
+});
+
+user.on('change', () => {
+  console.log('Change #2 triggered!');
+});
+
+user.on('save', () => {
+  console.log('Save was triggered!');
+});
+
+// No error on trigger any random unregistered event
+user.trigger('dsjhfkjds');
--- src/models/User.ts
+++ src/models/User.ts
@@ -20,11 +20,23 @@ class User {
   }
 
   on(eventName: string, callback: Callback): void {
-    // this.events[eventName]  = this.events[eventName] || [];
+    // Initially there is no any value in the events object, we need to set any truthy value [], so could not get any error or undefined
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
 
     this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const callbacks = this.events[eventName];
+
+    // Check, if callbacks exists and there is any callback inside callbacks array, if not, just return out the trigger function
+    if (!callbacks || !callbacks.length) return;
+
+    // If there are callbacks then simply executive them one by one
+    callbacks.forEach(callback => {
+      callback();
+    });
   }
 }
 

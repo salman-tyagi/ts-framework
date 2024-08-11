39b1b981288b55aead99d310b958f3126482b182
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-11 18:52:24 +0530

    refactor all user events into a new Eventing class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import User from './models/User';
 
 const user = new User({ name: 'Captain America', age: 34 });
+
+user.events.on('change', () => console.log('change triggered'));
+user.events.trigger('change');
+
 console.log(user);

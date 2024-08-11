06d585dc441871e1fe3358e10abaecbc7e95791f
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-11 18:40:50 +0530

    extract events into the new Eventing class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import User from './models/User';
 
-const user = new User({ id: 3, name: 'Captain America', age: 34 });
-user.save();
-
+const user = new User({ name: 'Captain America', age: 34 });
+console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -6,12 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-// Type alias as a TYPE, to make understand programmers
-type Callback = () => void;
-
 class User {
-  events: { [event: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -20,26 +15,6 @@ class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    // Initially there is no any value in the events object, we need to set any truthy value [], so could not get any error or undefined
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const callbacks = this.events[eventName];
-
-    // Check, if callbacks exists and there is any callback inside callbacks array, if not, just return out the trigger function
-    if (!callbacks || !callbacks.length) return;
-
-    // If there are callbacks then simply executive them one by one
-    callbacks.forEach(callback => {
-      callback();
-    });
   }
 
   fetch(): void {
@@ -54,17 +29,9 @@ class User {
     const id = this.get('id');
 
     if (!id) {
-      axios
-        .post('http://localhost:3000/users', this.data)
-        .then((res: AxiosResponse) => {
-          this.set(res.data);
-        });
+      axios.post('http://localhost:3000/users', this.data);
     } else {
-      axios
-        .patch(`http://localhost:3000/users/${id}`, this.data)
-        .then((res: AxiosResponse) => {
-          this.set(res.data);
-        });
+      axios.patch(`http://localhost:3000/users/${id}`, this.data);
     }
   }
 }

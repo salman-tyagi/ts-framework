c4c4d098744f8f1bc5acefe00840fb838c88a24b
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-15 12:47:12 +0530

    bind this keyword in Attributes class to the lexical scope

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import User from './models/User';
 
-const user = new User({ name: 'myName', age: 2 });
+const user = new User({ name: 'myName', age: 20 });
 
-user.on('click', () => console.log('on click event'));
-user.trigger('click');
+console.log(user.get('name'));
--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,9 +1,9 @@
 class Attributes<T extends object> {
   constructor(public data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     Object.assign(this.data, update);

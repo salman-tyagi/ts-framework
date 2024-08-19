95ea3a9bd0e69d85832b38aee23923c401889a60
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-19 10:35:43 +0530

    bind eventHandlers on generated HTML template

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,39 @@
 class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHoverH1,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Button clicked');
+  }
+
+  onHoverH1(): void {
+    console.log('h1 hovered');
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   template(): string {
     return `
       <div>
         <h1>User form</h1>
         <input />
+        <button>Click me</button>
       </div>
     `;
   }
@@ -14,6 +42,7 @@ class UserForm {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }

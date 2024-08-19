fa5d83905fd2086fbcd46d856e28cbe4da396682
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-19 10:01:48 +0530

    added UserForm class for the views and render created html

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-import User, { UserProps } from './models/User';
-import Collection from './models/Collection';
+import UserForm from './views/UserForm';
 
-const collection = new Collection<User, UserProps>(
-  'http://localhost:3000/users',
-  (json: UserProps) => User.buildUser(json)
-);
+const userForm = new UserForm(document.getElementById('root') as Element);
 
-collection.on('change', () => {
-  console.log(collection);
-  ``;
-});
-
-collection.fetch();
+userForm.render();
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import ApiSync from './ApiSync';
 import Attributes from './Attributes';
 import Eventing from './Eventing';
 import Model from './Model';
+import Collection from './Collection';
 
 const API_URL: string = 'http://localhost:3000/users';
 
@@ -17,6 +18,12 @@ class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(API_URL)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(API_URL, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
